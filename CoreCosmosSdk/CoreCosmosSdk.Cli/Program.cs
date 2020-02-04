5c2270ce21615c8336b032fce712e608d2bd0dc3
Andrew Withers <andreww@thomasdev.co.uk>
2020-02-04 09:55:24 +0000

    Add cosmos client usage example

--- CoreCosmosSdk/CoreCosmosSdk.Cli/Program.cs
+++ CoreCosmosSdk/CoreCosmosSdk.Cli/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
 
 namespace CoreCosmosSdk.Cli
 {
@@ -6,7 +9,39 @@ namespace CoreCosmosSdk.Cli
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            QueryForDocuments().Wait();
+        }
+
+        private static async Task QueryForDocuments()
+        {
+            var config = new ConfigurationBuilder()
+                                .AddJsonFile("appsettings.json")
+                                .Build();
+
+            var endpoint = config["cosmosDb:endpoint"];
+            var key = config["cosmosDb:key"];
+
+            using (var client = new CosmosClient(endpoint, key))
+            {
+                var query = "SELECT * FROM c WHERE ARRAY_LENGTH(c.children) > 1";
+
+                var container = client.GetContainer("Families", "Families");
+
+                // to perform a query, we first need to define an iterator
+                // use "dynamic" when there is no document type defined (probably always should be?)
+                var iterator = container.GetItemQueryIterator<dynamic>(query);
+
+                // we then fetch a page of results
+                var page = await iterator.ReadNextAsync();
+
+                // now we can iterate through the paged result
+                foreach (var doc in page)
+                {
+                    Console.WriteLine($"Family {doc.id} has {doc.children.Count} children");
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
